160923e54971c474e3b4928160081bc95bec628a
aparna4scholas <aparna4scholas@gmail.com>
2020-04-22 10:33:27 -0500

    added data to app

--- bookstoreapp/src/App.js
+++ bookstoreapp/src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">  My Children's Book Store
-        </header>
+       My Children's Book Store
+       
     </div>
   );
 }
