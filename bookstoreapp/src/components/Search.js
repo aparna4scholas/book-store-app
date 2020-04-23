282dbe876c30acb8ad8959f5a52b946aa7f23b0e
aparna4scholas <aparna4scholas@gmail.com>
2020-04-23 18:12:47 -0500

    modified

---
+++ bookstoreapp/src/components/Search.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Search () {
+    return (
+        <div className="">
+        <label className="label_1"> Email</label>
+        <input type="text" name="Email" placeholder=""/>
+        
+        </div>
+            )
+
+    }
+    
+
+export default Login;
