7984af0fa9d056d909731d7ac682c2283161294c
aparna4scholas <aparna4scholas@gmail.com>
2020-04-23 18:15:25 -0500

    modified

--- bookstoreapp/src/components/Search.js
+++ bookstoreapp/src/components/Search.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Search () {
     return (
         <div className="div_first">
-        <label className="label_first"> Email</label>
-        <input type="text" name="Email" placeholder=""/>
+        <label className="label_first"> Search</label>
+        <input type="text" name="Search" placeholder="bookname"/>
         
         </div>
             )
