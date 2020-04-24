ae63afd50ec733c7649fb9662ccb863e1a2d2f81
aparna4scholas <aparna4scholas@gmail.com>
2020-04-24 13:10:01 -0500

    modified

--- bookstoreapp/src/components/Search.js
+++ bookstoreapp/src/components/Search.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom'
+import axios from 'axios'
 
  class Search extends Component{
-    displayResults() {
-        
+    state = {
+        books : []
     }
-     
+
+    /* constructor (props){
+         super(props);
+         this.state = searchkey
+     }
+    displayResults =(e) => { 
+         
+       e.preventDefault()
+
+    }
+    textresult = (e) => {
+        e.preventDefault()
+    }
+ */
     
  
   render() {
@@ -13,10 +27,11 @@ import ReactDom from 'react-dom'
     return (
         <div className="div_first">
             <h1><label className="label"> Search</label></h1>
-            <input type="text" className="form-control" name="Search" placeholder="bookname" />
+            <input type="text" onChange={this.textresult} className="form-control" name="Search" placeholder="bookname" />
             <div className="div_second">
                 <button onClick={this.displayResults} className="btn btn-primary">submit</button>
             </div>
+            this.
             <div className="results-container">
             </div>
         </div>
