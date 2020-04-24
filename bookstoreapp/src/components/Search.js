ff87895197b39770e6eaf0f1ae1d91925814666b
aparna4scholas <aparna4scholas@gmail.com>
2020-04-24 00:10:27 -0500

    modified

--- bookstoreapp/src/components/Search.js
+++ bookstoreapp/src/components/Search.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import Search from "./components/search";
 
-function Search () {
+class App  extends React.Component { 
+    render() {
     return (
         <div className="div_first">
         <label className="label_first"> Search</label>
         <input type="text" name="Search" placeholder="bookname"/>
         </div>
 
-        )
+        );
 
     }
     
