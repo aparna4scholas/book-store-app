dfd4616971ce8c113afd06e45fe27f376f9b7a0a
aparna4scholas <aparna4scholas@gmail.com>
2020-04-25 19:17:15 -0500

    modified

--- bookstoreapp/src/components/Search.js
+++ bookstoreapp/src/components/Search.js
@@ -3,53 +3,62 @@ import ReactDom from 'react-dom'
 import axios from 'axios';
 const API_URL = 'https://bookauthority.org/api/site/get-topic-data?topicId=rest_api&viewId=best&formatId=print&featureId=null&skip=5&limit=10&getAdditionalBooks=false&getInfo=false&pageviewId=56023790858211ea9e710d1d42024a5c';
 
- class Search extends Component{
+class Search extends Component {
     state = {
-        searchString : "Bookname",
-        books : [],
-        booksall : []
+        searchString: "",
+        books: [],
+        booksall: []
     }
 
-    constructor (props){
-         super(props);
+    constructor(props) {
+        super(props);
 
-         this.getData = this.getData.bind(this);
-     }
-
-   getData() {
-       
-    alert(this.state.searchString);
-    
-       const url = `${API_URL}/users/`;
-       axios.get(url).then(response => response.data)
-       .then((data) => {
-           this.setState( {books: data.books})
-
-       })
+        this.getData = this.getData.bind(this);
+        this.setSerachVal = this.setSerachVal.bind(this);
     }
-    
- 
-  render() {
-      
-    return (
-        <div className="div_first">
-            <h1><label className="label"> Search</label></h1>
-            <input type="text" className="form-control" value={this.state.searchString} name="Search" placeholder="bookname" />
-            <div className="div_second">
-                <button onClick={this.getData} className="btn btn-primary">submit</button>
-            </div>
-            {this.state.books.map((book) => (
-             <div className="results-container">
-                 {book._id}
-                 <br></br>
-                 <img id={book._id} src={book.images.front.l.url} alt="No value"/>
-            </div>
-            ))}
-        </div>
-    
-    )
-  }
+
+    setSerachVal(searchbox) {
+        console.log(searchbox.target.value);
+        this.setState({searchString : searchbox.target.value});
 
     }
+
+    getData() {
+
+        alert(this.state.searchString);
+
+        const url = `${API_URL}/users/`;
+        axios.get(url).then(response => response.data)
+            .then((data) => {
+                this.setState({ books: data.books })
+
+            })
+    }
+
+
+    render() {
+
+        return (
+            <form>
+                <div className="div_first">
+                    <h1><label className="label"> Search</label></h1>
+                    <input type="text" className="form-control" onChange={this.setSerachVal} name="Search" placeholder="bookname" />
+                    <div className="div_second">
+                        <button onClick={this.getData} className="btn btn-primary">submit</button>
+                    </div>
+                    {this.state.books.map((book) => (
+                        <div className="results-container">
+                            {book._id}
+                            <br></br>
+                            <img id={book._id} src={book.images.front.l.url} alt="No value" />
+                        </div>
+                    ))}
+                </div>
+            </form>
+
+        )
+    }
+
+}
 export default Search;
 
